c3016505984ec72fa53b168f0f832fa443899ae1
Nathan Pham <nathanpham.me@gmail.com>
2021-10-14 23:04:01 -0700

    resize event listener

--- js/classes/Canvas.js
+++ js/classes/Canvas.js
@@ -4,7 +4,17 @@ export default class Canvas {
 
         this.root = typeof root == "string" ? document.querySelector(root) : root
         this.root.appendChild(this.#createCanvas())
+        this.#addEventListeners()
 
+    }
+
+    get dimensions() {
+
+        return {
+            width: this.root.offsetWidth,
+            height: this.root.offsetHeight
+        }
+            
     }
 
     #createCanvas() {
@@ -12,6 +22,26 @@ export default class Canvas {
         this.canvas = document.createElement("canvas")
         this.ctx = this.canvas.getContext("2d")
         return this.canvas
+
+    }
+
+    #addEventListeners() {
+
+        this.canvas.addEventListener("resize", () => {
+
+            const { width, height } = this.dimensions
+            const { canvas, ctx } = this
+
+            if(window.devicePixelRatio > 1) {
+                canvas.width = canvas.clientWidth * 2
+                canvas.height = canvas.clientHeight * 2
+                ctx.scale(2, 2)
+            } else {
+                canvas.width = dimensions.width
+                canvas.height = dimensions.height
+            }
+
+        })
 
     }
 
